79157c6204e2ecc22233faee1d2f320412e2af9f
Franco Tallei <ftallei.work@gmail.com>
2022-05-04 07:51:53 -0300

    Ejercicio de la Seccion 01

---
+++ src/com/example/Seccion01.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example;
+
+/**
+ *
+ * @author Tallei Franco
+ */
+public class Seccion01 {
+
+    public static void main(String[] args){
+
+        byte numeroByte = 1;
+        short numeroShort = 10;
+        int numero = 1000;
+        long numeroLargo = 1500000000;
+        float numeroDecimal = 1.4f;
+        double numeroDecimal2 = 1.55d;
+        boolean falso = false;
+        char caracter = 'c';
+        String texto = "texto";
+        Integer num = null;
+
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+        System.out.println(numero);
+        System.out.println(numeroLargo);
+        System.out.println(numeroDecimal);
+        System.out.println(numeroDecimal2);
+        System.out.println(falso);
+        System.out.println(texto);
+        System.out.println(caracter);
+        System.out.println(num);
+
+    }
+
+}
